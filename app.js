8756e106c4ced2f6149f104db80260478eb669cc
niedjaraujos <163071319+niedjaraujos@users.noreply.github.com>
2025-02-05 17:18:24 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-let inputDisplay = document.querySelector('input');
-let currentNumber = '';
 
-
-function takeValue(calculator) {
-    currentNumber += calculator
-    inputDisplay.value = currentNumber;
-    // Adiciona valores ao visor
+let botaoAdd = document.getElementById('display')
+function mostrarElemento(element) {
+    botaoAdd.value += element // += itera o novo numero sem subscrever o outro
+}
+function limpar() {
+    botaoAdd.value = ''
+}
+function apagar() {
+    botaoAdd.value = botaoAdd.value.slice(0, -1)
 }
 
-function cleanLast() {
-    currentNumber = currentNumber.slice(0, -1); // Remove o último caractere
-    inputDisplay.value = currentNumber; // Atualiza o visor
-}
+function calcular() {
 
-function calculate() {
     try {
-        let result = eval(currentNumber); // Avalia a expressão
-        inputDisplay.value = result.toFixed(2); // Mostra o resultado com 2 casas decimais
-        currentNumber = ''; // Reseta o número atual
-    } catch {
-        inputDisplay.value = 'Erro'; // Exibe erro se a expressão for inválida
-        currentNumber = ''; // Reseta o número atual
+        botaoAdd.value = eval(botaoAdd.value)
     }
-}
-
-function cleanDisplay() {
-    inputDisplay.value = ''; // Reseta o valor do visor
-    currentNumber = ''; // Reseta o número atual
+    catch {
+        botaoAdd.value = 'erro'
+    }
 }
